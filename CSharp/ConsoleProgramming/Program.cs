4a40ed946a6c3aabc06fa77d12a06da9cb9e0717
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-22 21:33:05 +0300

    Created console application

---
+++ CSharp/ConsoleProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

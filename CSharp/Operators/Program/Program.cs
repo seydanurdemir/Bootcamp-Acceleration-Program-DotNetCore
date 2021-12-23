057aaadbd6fc6ac5d4dd497fe51ca648725878c5
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-24 02:27:20 +0300

    Edited program

--- CSharp/Operators/Program/Program.cs
+++ CSharp/Operators/Program/Program.cs
@@ -6,19 +6,41 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Arithmetic Operators\n");
+            Console.WriteLine("Addition (+)");
+            Console.WriteLine("Subtraction (-)");
+            Console.WriteLine("Multiplication (*)");
+            Console.WriteLine("Divison (/)");
+            Console.WriteLine("Modulus (%)");
+            Console.WriteLine("Increment (++)");
+            Console.WriteLine("Decrement (--)");
 
-            Console.WriteLine("Assign, Operation Operator");
-            Console.WriteLine("=, +=, -=, *=, /=");
+            Console.WriteLine();
 
-            Console.WriteLine("Logical");
-            Console.WriteLine("||, &&, !");
+            Console.WriteLine("Relational Operators\n");
+            Console.WriteLine("Greater than (>)");
+            Console.WriteLine("Less than (<)");
+            Console.WriteLine("Equal to (==)");
+            Console.WriteLine("Greater than or equals to (>=)");
+            Console.WriteLine("Less than or equals to (<=)");
+            Console.WriteLine("Not equal (!=)");
 
-            Console.WriteLine("Relational");
-            Console.WriteLine("==,!=, <, >, >=, <=");
+            Console.WriteLine();
 
-            Console.WriteLine("Arithmetic");
-            Console.WriteLine("+, -, *, /, %, ++, --");
+            Console.WriteLine("Assignment Operators\n");
+            Console.WriteLine("Equals to (=)");
+            Console.WriteLine("Add equal to (+=)");
+            Console.WriteLine("Subtract equal to (-=)");
+            Console.WriteLine("Multiply equal to (*=)");
+            Console.WriteLine("Divide equal to (/=)");
+            Console.WriteLine("Modulus equal to (%=)");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Logical Operators\n");
+            Console.WriteLine("And (&&)");
+            Console.WriteLine("Or (||)");
+            Console.WriteLine("Not (!)");
         }
     }
 }

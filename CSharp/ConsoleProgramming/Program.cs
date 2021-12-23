ce798d236a8ccc67457b15e53fa1455bbfcfc125
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-23 14:36:48 +0300

    Edited program cs

--- CSharp/ConsoleProgramming/Program.cs
+++ CSharp/ConsoleProgramming/Program.cs
@@ -7,6 +7,11 @@ namespace ConsoleProgramming
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Isminizi Giriniz : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Giriniz : ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba " + name + surname + " !");
         }
     }
 }

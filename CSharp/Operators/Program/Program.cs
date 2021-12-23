359dc47d5010e8609eabea65bf27c2a741f8d414
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-24 02:16:24 +0300

    Edited program

--- CSharp/Operators/Program/Program.cs
+++ CSharp/Operators/Program/Program.cs
@@ -7,6 +7,18 @@ namespace Program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Assign, Operation Operator");
+            Console.WriteLine("=, +=, -=, *=, /=");
+
+            Console.WriteLine("Logical");
+            Console.WriteLine("||, &&, !");
+
+            Console.WriteLine("Relational");
+            Console.WriteLine("==,!=, <, >, >=, <=");
+
+            Console.WriteLine("Arithmetic");
+            Console.WriteLine("+, -, *, /, %, ++, --");
         }
     }
 }

daa2713ee52986d82bc1f8a44e8806cb752b6401
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-25 18:42:42 +0300

    Edited program

--- CSharp/Loops/Program/Program.cs
+++ CSharp/Loops/Program/Program.cs
@@ -7,6 +7,39 @@ namespace Program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            try
+            {
+                Console.WriteLine("Bir sayi giriniz : ");
+                int sayi = int.Parse(Console.ReadLine());
+
+                int tekToplam = 0;
+                int ciftToplam = 0;
+
+                for (int i = 1; i <= sayi; i++)
+                {
+                    if (i % 2 == 1)
+                    {
+                        tekToplam += i;
+                    }
+                    else
+                    {
+                        ciftToplam += i;
+                    }
+                }
+
+                Console.WriteLine("1 ile " + sayi + " arasindaki tek sayilarin toplami : " + tekToplam);
+                Console.WriteLine("1 ile " + sayi + " arasindaki cift sayilarin toplami : " + ciftToplam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Calculated");
+            }
+
         }
     }
 }

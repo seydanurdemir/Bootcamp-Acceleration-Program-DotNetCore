c8b8ad0986378b87c2b4fd3706cb044c5e0c6a3f
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-26 00:44:18 +0300

    Edited program

--- CSharp/8-Loops/Program/Program.cs
+++ CSharp/8-Loops/Program/Program.cs
@@ -10,13 +10,11 @@ namespace Program
             Console.WriteLine("For");
             Console.WriteLine();
 
-            Console.Write("Bir sayi giriniz : ");
-            int sayi = int.Parse(Console.ReadLine());
             Console.WriteLine();
 
             int tekToplam = 0;
 
-            for (int i = 1; i <= sayi; i++)
+            for (int i = 1; i <= 20; i++)
             {
                 if (i % 2 == 1)
                 {
@@ -26,7 +24,21 @@ namespace Program
             }
 
             Console.WriteLine();
-            Console.WriteLine("1 ile " + sayi + " arasindaki tek sayilarin toplami : " + tekToplam);
+            Console.WriteLine("1 ile 20 arasindaki tek sayilarin toplami : " + tekToplam);
+
+            Console.WriteLine();
+            Console.WriteLine("Continue - Break");
+            Console.WriteLine();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 5 == 0)
+                    continue;
+                if (i == 17)
+                    break;
+
+                Console.WriteLine(i);
+            }
 
             Console.WriteLine();
         }

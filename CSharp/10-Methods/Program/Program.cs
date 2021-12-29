c8cbc31ab6a4522ba064d14fff877fd6b3dc45e3
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-29 05:59:23 +0300

    Edited program

--- CSharp/10-Methods/Program/Program.cs
+++ CSharp/10-Methods/Program/Program.cs
@@ -43,6 +43,57 @@ namespace Program
         }
     }
 
+    public static class ExtensionMethods
+    {
+        public static void Print<T>(this T[] arr)
+        {
+            // System.Collections.IEnumerator enum = arr.GetEnumerator();
+            // while ((enum.MoveNext()) && (enum.Current != null))
+            //     Console.Write(enum.Current + " ");
+
+            foreach (T el in arr)
+            {
+                Console.Write(el + " ");
+            }
+        }
+
+        public static string RemoveWhiteSpaces(this string str)
+        {
+            return string.Join("", str.Split(" "));
+        }
+    }
+
+    public class RecursiveMethods
+    {
+        public static long GCD(long number1, long number2)
+        {
+            if (number2 == 0)
+                return number1;
+            else
+                return GCD(number2, number1 % number2);
+        }
+
+        // // Not Recursive
+        // public static long LCM(long number1, long number2)
+        // {
+        //     return (number1 * number2) / GCD(number1, number2);
+        // }
+
+        public static long Factorial(long number)
+        {
+            if (number == 0)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+
+        public static long Fibonacci(long number)
+        {
+            if (number <= 1)
+                return 1;
+            return Fibonacci(number - 1) + Fibonacci(number - 2);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -86,6 +137,38 @@ namespace Program
 
             RefOut obj4 = new RefOut();
             Console.WriteLine(obj4.IsQuarter(out int min) + " : " + min);
+
+            Console.WriteLine();
+            Console.WriteLine("Extension Methods");
+            Console.WriteLine();
+
+            int[] arr = { 10, 15, 20, 25, 30 };
+            // Static Method Usage
+            ExtensionMethods.Print<int>(arr);
+            Console.WriteLine();
+            // Extension Method Usage
+            arr.Print<int>();
+            Console.WriteLine();
+
+            Console.WriteLine();
+            string snt = "how are you doing today";
+            Console.WriteLine(snt.RemoveWhiteSpaces());
+
+            Console.WriteLine();
+            Console.WriteLine("Recursive Methods");
+            Console.WriteLine();
+
+            // int num = 98765;
+            // string str = num.ToString();
+            // char[] chr = str.ToCharArray();
+
+            int number = 5;
+            int number1 = 24;
+            int number2 = 16;
+            Console.WriteLine("GCD ({0},{1}) : {2}", number1, number2, RecursiveMethods.GCD(number1, number2));
+            //Console.WriteLine("LCM ({0},{1}) : {2}", number1, number2, RecursiveMethods.LCM(number1, number2)); // Not Recursive
+            Console.WriteLine("Factorial ({0}) : {1}", number, RecursiveMethods.Factorial(number));
+            Console.WriteLine("Fibonacci ({0}) : {1}", number, RecursiveMethods.Fibonacci(number));
 
             Console.WriteLine();
         }

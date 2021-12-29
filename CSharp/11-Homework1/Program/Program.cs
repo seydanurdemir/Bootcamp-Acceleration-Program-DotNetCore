27b6f9d832e2428de691ed3ce6f6bf478e1dcb2d
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2021-12-29 15:11:04 +0300

    Edited program

--- CSharp/11-Homework1/Program/Program.cs
+++ CSharp/11-Homework1/Program/Program.cs
@@ -2,11 +2,41 @@
 
 namespace Program
 {
+    public class Homework1
+    {
+        public static void Part1()
+        {
+            Console.WriteLine("Please enter a length : ");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Please enter a number : ");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if ((arr[i] % 2) == 0)
+                    Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Homework 1");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1");
+            Console.WriteLine();
+
+            Homework1.Part1();
+
+            Console.WriteLine();
         }
     }
 }

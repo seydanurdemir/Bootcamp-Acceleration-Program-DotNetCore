4ed19edf634d51dfb5a280425d790d49de0fdcb2
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-06 00:30:58 +0300

    Edited program

--- CSharp/14-Homework2/Program/Program.cs
+++ CSharp/14-Homework2/Program/Program.cs
@@ -102,10 +102,115 @@ namespace Program
 
         public static void Part2()
         {
+            int size = 20;
+            ArrayList numberList = new ArrayList();
+
+            Console.WriteLine("Taking " + size + " Elements");
+            Console.WriteLine();
+
+            int el = 0;
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write((i + 1) + ". Element : ");
+                el = int.Parse(Console.ReadLine());
+                numberList.Add(el);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Print List");
+            Console.WriteLine();
+
+            foreach (var item in numberList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sorting & Reversing List");
+            Console.WriteLine();
+
+            int min1, min2, min3, minAvg;
+            numberList.Sort();
+            min1 = (int)numberList[0];
+            min2 = (int)numberList[1];
+            min3 = (int)numberList[2];
+            minAvg = ((min1 + min2 + min3) / 3);
+
+            Console.WriteLine("Minimum 1 : " + min1);
+            Console.WriteLine("Minimum 2 : " + min2);
+            Console.WriteLine("Minimum 3 : " + min3);
+            Console.WriteLine();
+
+            Console.WriteLine("Minimum Average : " + minAvg);
+            Console.WriteLine();
+
+            int max1, max2, max3, maxAvg;
+            numberList.Reverse();
+            max1 = (int)numberList[0];
+            max2 = (int)numberList[1];
+            max3 = (int)numberList[2];
+            maxAvg = ((max1 + max2 + max3) / 3);
+
+            Console.WriteLine("Maximum 1 : " + max1);
+            Console.WriteLine("Maximum 2 : " + max2);
+            Console.WriteLine("Maximum 3 : " + max3);
+            Console.WriteLine();
+
+            Console.WriteLine("Maximum Average : " + maxAvg);
+            Console.WriteLine();
+
+            int sumAvg = minAvg + maxAvg;
+
+            Console.WriteLine("Sum of Averages : " + sumAvg);
+            Console.WriteLine();
+        }
+
+        public static bool isVowel(char letter)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y' };
+
+            if (Array.IndexOf(vowels, letter) != -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public static void Part3()
         {
+            Console.WriteLine("Taking Sentences");
+            Console.WriteLine();
+
+            string sentences = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Create Vowels List");
+            Console.WriteLine();
+
+            char[] letters = sentences.ToCharArray();
+            List<char> vowels = new List<char>(letters);
+            vowels.RemoveAll(el => (!isVowel(el)));
+
+            Console.WriteLine("Print List");
+            Console.WriteLine();
+
+            vowels.ForEach(el => Console.WriteLine(el));
+            Console.WriteLine();
+
+            Console.WriteLine("Sort List");
+            Console.WriteLine();
+
+            vowels.Sort();
+
+            Console.WriteLine("Print Sorted List");
+            Console.WriteLine();
+
+            vowels.ForEach(el => Console.WriteLine(el));
+            Console.WriteLine();
+
         }
     }
     class Program

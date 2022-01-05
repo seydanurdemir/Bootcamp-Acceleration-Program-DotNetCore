9c85a18413032cf20d088615362aa77a9b84bc29
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-06 00:49:44 +0300

    Created class project

---
+++ CSharp/15-Class/Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Class");
+            Console.WriteLine();
+
+            //
+
+            Console.WriteLine();
+        }
+    }
+}

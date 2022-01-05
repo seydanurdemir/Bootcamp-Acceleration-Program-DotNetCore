76306aad335103202718fd9e3e581657b8a181a6
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-05 19:06:03 +0300

    Edited program

--- CSharp/14-Homework2/Program/Program.cs
+++ CSharp/14-Homework2/Program/Program.cs
@@ -1,11 +1,103 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Program
 {
     public class Homework2
     {
+        public static bool isPrime(int number)
+        {
+            if ((number == 0) || (number == 1))
+            {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
         public static void Part1()
         {
+            int size = 20;
+            ArrayList primeList = new ArrayList();
+            ArrayList nonprimeList = new ArrayList();
+
+            Console.WriteLine("Taking " + size + " Elements");
+            Console.WriteLine();
+
+            int el = 0;
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write((i + 1) + ". Element : ");
+                el = int.Parse(Console.ReadLine());
+                if (isPrime(el))
+                {
+                    primeList.Add(el);
+                }
+                else
+                {
+                    nonprimeList.Add(el);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Sorting & Reversing Lists");
+            Console.WriteLine();
+
+            primeList.Sort();
+            primeList.Reverse();
+
+            nonprimeList.Sort();
+            nonprimeList.Reverse();
+
+            Console.WriteLine("Prime List");
+            Console.WriteLine();
+
+            foreach (var item in primeList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Count : " + primeList.Count);
+            Console.WriteLine();
+
+            int primeSum = 0;
+            foreach (var item in primeList)
+            {
+                primeSum += (int)item;
+            }
+
+            Console.WriteLine("Average : " + ((primeList.Count != 0) ? (primeSum / primeList.Count) : 0));
+            Console.WriteLine();
+
+            Console.WriteLine("Non-Prime List");
+            Console.WriteLine();
+
+            foreach (var item in nonprimeList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Count : " + nonprimeList.Count);
+            Console.WriteLine();
+
+            int nonprimeSum = 0;
+            foreach (var item in nonprimeList)
+            {
+                nonprimeSum += (int)item;
+            }
+
+            Console.WriteLine("Average : " + ((nonprimeList.Count != 0) ? (nonprimeSum / nonprimeList.Count) : 0));
+            Console.WriteLine();
         }
 
         public static void Part2()

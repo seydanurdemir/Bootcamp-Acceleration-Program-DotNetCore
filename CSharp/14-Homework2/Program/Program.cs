005842a71aea7fe4cd57d3745ba84b91160e566c
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-01-05 18:35:24 +0300

    Edited program

--- CSharp/14-Homework2/Program/Program.cs
+++ CSharp/14-Homework2/Program/Program.cs
@@ -2,11 +2,47 @@
 
 namespace Program
 {
+    public class Homework2
+    {
+        public static void Part1()
+        {
+        }
+
+        public static void Part2()
+        {
+        }
+
+        public static void Part3()
+        {
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Homework 1");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 1 --------------------");
+            Console.WriteLine();
+
+            Homework2.Part1();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 2 --------------------");
+            Console.WriteLine();
+
+            Homework2.Part2();
+
+            Console.WriteLine();
+            Console.WriteLine("Part 3 --------------------");
+            Console.WriteLine();
+
+            Homework2.Part3();
+
+            Console.WriteLine();
         }
     }
 }

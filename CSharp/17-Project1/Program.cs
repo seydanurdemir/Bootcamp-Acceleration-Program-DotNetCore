13416e72c379c1eb0bef20183c37475c56007e6f
Şeyda Nur Demir <57226488+seydanurdemir@users.noreply.github.com>
2022-02-11 05:28:16 +0300

    Created project 1 project

---
+++ CSharp/17-Project1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _17_Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
